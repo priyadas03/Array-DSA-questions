944a83a454f7888ada1035237924a8a6805ac091
PRIYA DAS <queenpriyadas2003@gmail.com>
2024-07-04 12:44:57 +0530

    Add files via upload

---
+++ reverse_array.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void reverse(int arr[],int n){
+    int start=0;
+    int end=n-1;
+    while(start<=end){
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
+    }
+
+}
+void printarray(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    reverse(arr,n);
+    printarray(arr,n);
+}
